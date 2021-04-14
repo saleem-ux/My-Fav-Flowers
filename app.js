2de4d18741479e677ad2e0e92c187d7c08f12524
SaleemDiab <>
2021-04-14 13:48:22 +0000

    hello

---
+++ app.js
@@ -0,0 +1,11 @@
+var userNumber=prompt('Please enter your lovely number')
+if(userNumber >=10){
+  document.write('Orange lily');
+  document.write( '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Liliumbulbiferumflowertop.jpg/220px-Liliumbulbiferumflowertop.jpg" >' );
+}else if(userNumber >= 5){
+  document.write('Peach flower');
+  document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Peach_flowers.jpg">');
+}else if(userNumber >= 0){
+  document.write('Plumeria');
+  document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Frangipani_flowers.jpg">');
+}
