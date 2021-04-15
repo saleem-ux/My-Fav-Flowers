f14637ed59a7f02c0386b69e893533f7a231e666
SaleemDiab <>
2021-04-15 11:05:01 +0000

    read05

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ if(userNumber >=10){
   document.write('Plumeria');
   document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Frangipani_flowers.jpg">');
 }
+var flowerName = prompt('Hello, please enter your favorite Flower name');
+while(flowerName !== 'orange lily' && flowerName !== 'peach flower' && flowerName !== 'plumeria'){
+  flowerName = prompt('Please enter the name plumeria or orange lily or peach flower');
+}
+var userChoose;
+if(flowerName === 'orange lily'){
+ 
+  userChoose='<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Liliumbulbiferumflowertop.jpg/220px-Liliumbulbiferumflowertop.jpg" >';
+}else if(flowerName === 'peach flower'){
+  
+  userChoose='<img src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Peach_flowers.jpg">';
+}else if(flowerName === 'plumeria'){
+  
+  userChoose='<img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Frangipani_flowers.jpg">';
+}
+
+var imagesNumber= prompt('How many image do you want?');
+for(var i=0 ; i < imagesNumber; i++){
+  document.write(userChoose);
+}
