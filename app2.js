b79149e9093fdd961a0d51a51da478eface0cbda
saleem diab <82366428+saleem-ux@users.noreply.github.com>
2021-04-26 03:53:55 -0700

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,13 @@
+let size = prompt('Prefer sport cars or family cars?');
+console.log(size);
+
+let model = prompt('Which car you like to book?');
+console.log(model);
+
+let color = prompt('Which color you prefer?');
+console.log(color);
+
+let year = prompt('Which year you prefer?');
+console.log(year);
+
+alert(' Hello you choose ' + size + ' car with ' + model + ' model you prefer ' + color + ' color with year model ' + year );
